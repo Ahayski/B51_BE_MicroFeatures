d02ac5ed3f0de05687b5754d139761174aa1c034
Ahayski <febryandegheans@gmail.com>
2024-01-07 22:08:51 +0700

    MicroFeatures with CORS CONFIG

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { AppDataSource } from "./data-source"
-import * as express from "express";
-import router from "./route";
+import * as express from "express"
+import * as cors from "cors"
+import router from "./route"
 import 'dotenv/config'
 
 AppDataSource.initialize()
     .then(async () => {
         const app = express()
+
+        app.use(cors({
+            origin: '*',
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        }))
 
         app.use(express.json())
         app.use("/api/v1", router)

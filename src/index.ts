46e704506195fddab4a7e5f502cfecb5affd7c4c
Ahayski <febryandegheans@gmail.com>
2024-01-08 00:03:09 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,14 @@ AppDataSource.initialize()
     .then(async () => {
         const app = express()
 
+        // CORS (Cross-Origin Resource Sharing) adalah kebijakan keamanan yang diterapkan oleh browser web 
+        // untuk melindungi pengguna dari potensi ancaman keamanan saat mereka mengakses sumber daya 
+        // di luar domain atau origin (asal) situs web yang sedang mereka kunjungi. Origin mencakup 
+        // protokol (http/https), domain, dan nomor port.
         app.use(cors({
             origin: '*',
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization']
         }))
 
         app.use(express.json())
